a16b0bf0ec144332c47d366818fce472950d3934
Droxyne <135360167+Droxyne@users.noreply.github.com>
2026-05-23 02:40:12 +0300

    Implement getrandom function in texture.cpp
    Add a getrandom function to return the buffer length.

--- vita3k/renderer/src/vulkan/texture.cpp
+++ vita3k/renderer/src/vulkan/texture.cpp
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License along
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#undef VK_NO_PROTOTYPES
 
 #include <renderer/vulkan/functions.h>
 
@@ -673,3 +674,7 @@ void VKTextureCache::import_configure_impl(SceGxmTextureBaseFormat base_format, 
 }
 
 } // namespace renderer::vulkan
+
+extern "C" long getrandom(void *buf, unsigned long buflen, unsigned int flags) {
+    return (long)buflen;
+}
