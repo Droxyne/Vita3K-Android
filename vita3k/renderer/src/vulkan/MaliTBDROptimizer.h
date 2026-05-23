2f0e20b1c33f0aa794bcddc24fdddeee7980aaa7
Droxyne <135360167+Droxyne@users.noreply.github.com>
2026-05-23 05:33:27 +0300

    Remove Vulkan prototypes from MaliTBDROptimizer.h
    Removed Vulkan prototype declarations from header.

--- vita3k/renderer/src/vulkan/MaliTBDROptimizer.h
+++ vita3k/renderer/src/vulkan/MaliTBDROptimizer.h
@@ -3,15 +3,6 @@
 #include <vector>
 #include <stdexcept>
 
-// Force-feed Vulkan prototypes directly to the compiler
-extern "C" {
-    VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceMemoryProperties(VkPhysicalDevice, VkPhysicalDeviceMemoryProperties*);
-    VKAPI_ATTR VkResult VKAPI_CALL vkCreateImage(VkDevice, const VkImageCreateInfo*, const VkAllocationCallbacks*, VkImage*);
-    VKAPI_ATTR void VKAPI_CALL vkGetImageMemoryRequirements(VkDevice, VkImage, VkMemoryRequirements*);
-    VKAPI_ATTR VkResult VKAPI_CALL vkAllocateMemory(VkDevice, const VkMemoryAllocateInfo*, const VkAllocationCallbacks*, VkDeviceMemory*);
-    VKAPI_ATTR VkResult VKAPI_CALL vkBindImageMemory(VkDevice, VkImage, VkDeviceMemory, VkDeviceSize);
-}
-
 
 class MaliTBDROptimizer {
 public:
