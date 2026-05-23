527a6b3258e3b0229367cabb38c98039cdd0d1b5
Droxyne <135360167+Droxyne@users.noreply.github.com>
2026-05-23 15:53:43 +0300

    Remove MALI TBDR transient trap for depth buffers
    Removed MALI TBDR transient trap logic for depth stencil formats.

--- vita3k/renderer/src/vulkan/texture.cpp
+++ vita3k/renderer/src/vulkan/texture.cpp
@@ -391,14 +391,7 @@ void VKTextureCache::configure_texture(const SceGxmTexture &gxm_texture) {
         .sharingMode = vk::SharingMode::eExclusive,
         .initialLayout = vk::ImageLayout::eUndefined,
     };
-    // --- MALI TBDR TRANSIENT TRAP START ---
-    if (MaliTBDROptimizer::IsDepthStencilFormat(static_cast<VkFormat>(image_info.format))) {
-        // Force the driver to keep this depth buffer in the fast on-chip SRAM
-        image_info.usage &= ~vk::ImageUsageFlagBits::eSampled;
-        image_info.usage &= ~vk::ImageUsageFlagBits::eTransferSrc;
-        image_info.usage &= ~vk::ImageUsageFlagBits::eTransferDst;
-        image_info.usage |= vk::ImageUsageFlagBits::eTransientAttachment;
-    }
+    
     // --- MALI TBDR TRANSIENT TRAP END ---
 
     std::tie(image.image, image.allocation) = state.allocator.createImage(image_info, vkutil::vma_auto_alloc);
